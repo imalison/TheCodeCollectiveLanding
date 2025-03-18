220b953a5f2469fe0996760c9337569a5c3f3ed6
alisonapl <164436905+imalison@users.noreply.github.com>
2025-03-18 00:35:11 -0400

    Avance en el responsive

--- index.js
+++ index.js
@@ -34,3 +34,14 @@ document.addEventListener("DOMContentLoaded", () => {
         });
     }
 });
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    const menuToggle = document.querySelector(".menu-toggle");
+    const navLinks = document.querySelector(".nav-links");
+
+    menuToggle.addEventListener("click", function () {
+        navLinks.classList.toggle("active");
+    });
+});
+
